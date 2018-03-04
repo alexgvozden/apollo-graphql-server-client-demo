a305c5aca0069528b171c8b093c8264ce81448c6
alex.gvozden <alex.gvozden@gmail.com>
2018-03-04 20:37:53 +0100

    apollo 2 update to tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
+import { ApolloProvider, graphql } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { SchemaLink } from 'apollo-link-schema';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+
+import gql from 'graphql-tag';
+import { typeDefs } from './schema';
 import logo from './logo.svg';
 import './App.css';
+
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+
+const apolloCache = new InMemoryCache(window.__APOLLO_STATE__);
+
+const client = new ApolloClient({
+  cache: apolloCache,
+  link: new SchemaLink({ schema })
+});
+
+const ChannelsList = ({ data: { loading, error, channels } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  return <ul>{channels.map(ch => <li key={ch.id}>{ch.name}</li>)}</ul>;
+};
+const channelsListQuery = gql`
+  query ChannelsListQuery {
+    channels {
+      id
+      name
+    }
+  }
+`;
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Apollo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo</h2>
+          </div>
+          <ChannelsListWithData />
+        </div>
+      </ApolloProvider>
     );
   }
 }
